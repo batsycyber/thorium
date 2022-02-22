5bcf42c8eb0dca6b746d1eababdff478566fe506
batsycyber <alimohsin222111@gmail.com>
2022-02-22 17:59:27 +0530

    initial commit

---
+++ src/logger/logger.js
@@ -0,0 +1,13 @@
+let url = 'http://www.google.com'
+
+function log(message) {
+    console.log(message)
+}
+
+function Welcome() {
+    console.log("Welcom to my application")
+}
+module.exports.endpoint = url
+
+module.exports.printMessage = printMyMessage
+module.exports.printWelcomeMessage = Welcome
