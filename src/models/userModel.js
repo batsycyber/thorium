6e7692448f823130a9c886ce4b30f772887958ef
batsycyber <alimohsin222111@gmail.com>
2022-03-02 14:41:20 +0530

    mongo3done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
+const bookSchema = new mongoose.Schema({
+    name: String,
+    author_id : {
+        type:Number,
         required: true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
-}, { timestamps: true });
+    price : Number,
+    rating : Number
+},
+{timestamps:true})
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Bookslib',bookSchema)
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
 
 
 
-// String, Number
-// Boolean, Object/json, array
+// // String, Number
+// // Boolean, Object/json, array
